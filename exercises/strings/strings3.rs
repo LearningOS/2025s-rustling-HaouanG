abfcc86afcf09d2fe55b2697483b7b081ff00089
root <root@LENOVO.>
2025-04-01 12:01:44 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,28 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut removesapce :String=(&input).to_string();
+    removesapce=(removesapce.trim()).to_string();
+    removesapce
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut addworld :String=(&input).to_string();
+    addworld.push_str(" world!");
+    addworld
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    //???
+    let mut replacec :String=(&input).to_string();
+    replacec=replacec.replace("cars","balloons");
+    replacec
 }
 
 #[cfg(test)]
